68d8db9f725dcfb42340c0459b6c0de6d22f0c52
aadi-thedeyguy <adikhare24@gmail.com>
2023-01-09 23:32:28 +0530

    shpw saved expenses from database

--- src/components/Expense/Expenses.jsx
+++ src/components/Expense/Expenses.jsx
@@ -1,20 +1,49 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ExpenseForm from "./ExpenseForm"
 
 
 function Expenses() {
 
-    const [expenses, setExpenses] = useState([
-        {
-            amount: 200,
-            description: 'some description',
-            category: 'dummy category'
-        }
-    ])
+    const url = "https://fir-99cf8-default-rtdb.asia-southeast1.firebasedatabase.app/expenses.json"
 
-    const onAdd = (expense) => {
-        setExpenses([...expenses, expense])
-        console.log(expense, expenses)
+    const [expenses, setExpenses] = useState([])
+    useEffect(() => {
+     getExpenses()
+    }, [])
+    
+    const getExpenses = () => {
+        fetch(url).then(res => res.json())
+        .then(data => {
+          const arr = []
+          for (const key in data) {
+              arr.push({
+                  id : key,
+                  amount: data[key].amount,
+                  description: data[key].description,
+                  category : data[key].category
+              })
+          }
+          setExpenses(arr)
+        })
+    }
+
+    const onAdd = async (expense) => {
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(expense),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+        try {
+
+            const res = await fetch(url, options)
+            const data = await res.json()
+            // getExpenses()
+            setExpenses([...expenses, expense])
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     return (
